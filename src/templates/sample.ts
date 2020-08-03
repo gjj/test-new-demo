ebbcb3ae63183436717435cecf6aa81553f6aaa7
Goi Jia Jian <jiajiannn@gmail.com>
2020-08-03 19:44:13 +0800

    feat: test

--- src/templates/sample.ts
+++ src/templates/sample.ts
@@ -1,22 +1,20 @@
 import { Document } from "@govtechsg/decentralized-renderer-react-components";
 
-export interface CustomTemplateCertificate extends Document {
+export interface CocTemplateCertificate extends Document {
   name: string;
-  institute: string;
-  foo?: {
-    title: string;
+  recipient: {
+    name: string;
   };
 }
 
-export const customTemplateCertificate: CustomTemplateCertificate = {
-  name: "John Doe",
-  institute: "Institute of John Doe",
+export const cocTemplateCertificate: CocTemplateCertificate = {
+  name: "OpenAttestation Tutorial Certificate of Completion",
+  recipient: {
+    name: "John Doe"
+  },
   $template: {
-    name: "custom",
+    name: "COC",
     type: "EMBEDDED_RENDERER",
     url: "http://localhost:3000"
-  },
-  foo: {
-    title: "Bar is awesome"
   }
 };
